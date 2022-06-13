94ed57116785e8bfb415a03b8f2b094fcfcc267f
Andre Pedro <andrerpedro170@gmail.com>
2022-06-13 08:15:55 -0600

    create test for decrypt through the enigma class

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -28,4 +28,14 @@ RSpec.describe Enigma do
   end
 
 
+    it "can decrypt a message" do
+      expect(@enigma.encrypt("hello world", "02715", "040895")).to eq({
+        encryption: "hello world",
+        key: "02715",
+        date: "040895"})
+    end
+
+
+
+
 end
